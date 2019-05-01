e86f5cc7cc5eb5047549f1d08ad91803c4853258
Alexandre Pinho <lxpnh98@gmail.com>
2019-05-01 15:14:31 +0100

    Implement sequence and acknowledgement numbers (like TCP).

--- src/connection.py
+++ src/connection.py
@@ -13,10 +13,10 @@ class Connection():
         self.out_queue = q
         self.initialized = False
         self.first = False
-        self.acknowledged = 0
-        self.timeout = None
+
         self.file_id = 1
-        self.files = {}
+        self.files = { 0 : file.File(0, None, None) }
+
 
     def __repr__(self):
         return "(init: {}, first: {})".format(self.initialized, self.first)
@@ -31,53 +31,57 @@ class Connection():
     def receive(self):
         while True:
             p = self.in_queue.get()
+            self.files[p.file_id].ack_send(p.seq_num)
             if (p.flags[packet.ACK]):
-                self.acknowledged = p.ack_num
-                self.update_timeout()
+                self.files[p.file_id].ack_recv(p.ack_num)
             if (p.flags[packet.SYN]):
                 self.init(p)
                 if (self.first == False):
-                    p2 = packet.Packet(0, (True, False, True, False))
+                    p2 = packet.Packet(flags=(True, False, True, False))
                 else:
-                    p2 = packet.Packet(0, (False, False, True, False))
-                self.out_queue.put((self.dest_addr, p2))
+                    p2 = packet.Packet(flags=(False, False, True, False))
+                self.send_packet(p2)
             elif p.type == packet.GET:
                 if p.data == "":
                     print("no file")
                 else:
                     self.create_file(packet.GET, p.data)
 
-    def update_timeout(self):
-        if self.timeout:
-            self.timeout.cancel()
-        self.timeout = threading.Timer(5.0, print, ("timeout",))
-        self.timeout.start()
-
     def create_file(self, operation, path):
-        new_file_id = self.file_id
+        self.files[self.file_id] = file.File(self.file_id, operation, path)
         self.file_id += 1
-        self.files[new_file_id] = file.File(new_file_id, operation, path)
 
     def send(self):
         while True:
             for id in self.files.copy():
                 f = self.files[id]
                 if f.operation == packet.GET:
-                    sent = f.send_next(self.dest_addr, self.out_queue)
-                    if sent == 0: # envio completo 
+                    d = f.get_next_chunk()
+                    p = packet.Packet(packet.DATA, file_id=f.file_id, data=d)
+                    self.send_packet(p)
+                    if len(d) == 0: # envio completo 
                         self.files.pop(id)
+
+    def send_packet(self, p):
+        if p.seq_num == 0:
+            p.seq_num = self.files[p.file_id].get_next_seq_num()
+        p.ack_num = self.files[p.file_id].get_ack_num()
+        self.out_queue.put((self.dest_addr, p))
+        t = threading.Timer(5.0, self.send_packet, (p,))
+        self.files[p.file_id].packets_sending.append((p, t))
+        t.start()
 
     def get_dest(self):
         return self.dest_addr
 
     def get_file(self, path):
         p = packet.Packet(packet.GET, (False,)*4, data=path)
-        self.out_queue.put((self.dest_addr, p))
+        self.send_packet(p)
 
     def init(self, p):
         self.initialized = True
 
     def begin(self):
         self.first = True
-        p = packet.Packet(0, (True, False, False, False))
-        self.out_queue.put((self.dest_addr, p))
+        p = packet.Packet(packet.CONTROL, (True, False, False, False))
+        self.send_packet(p)
--- src/file.py
+++ src/file.py
@@ -4,17 +4,46 @@ class File:
     def __init__(self, file_id, operation, path):
         self.file_id = file_id
         self.operation = operation
+        self.seq_num = 1
+        self.ack_num = 0
+        self.packets_sending = []
+        self.acks_to_send = []
 
-        if operation == packet.GET:
-            mode = "r"
-        elif operation == packet.PUT:
-            mode = "w"
+        if file_id != 0:
+            if operation == packet.GET:
+                mode = "r"
+            elif operation == packet.PUT:
+                mode = "w"
+            else:
+                raise Exception("unknown operation {}".format(operation))
+            self.file = open(path, mode)
         else:
-            raise Exception("unknown operation {}".format(operation))
-        self.file = open(path, mode)
+            self.file = None
 
-    def send_next(self, dest_addr, queue):
-        d = self.file.read(1000)
-        p = packet.Packet(packet.DATA, file_id=self.file_id, data=d)
-        queue.put((dest_addr, p))
-        return len(d)
+    def get_next_seq_num(self):
+        s = self.seq_num
+        self.seq_num += 1
+        return s
+
+    def get_ack_num(self):
+        return self.ack_num
+
+    def get_next_chunk(self):
+        if self.file:
+            return self.file.read(1000)
+        else:
+            raise Exception("file not opened")
+
+    def ack_send(self, ack_num):
+        self.acks_to_send.append(ack_num)
+        for ack in sorted(self.acks_to_send):
+            if self.ack_num + 1 == ack:
+                self.ack_num = ack
+                self.acks_to_send.remove(ack)
+
+    def ack_recv(self, ack_num):
+        for p, t in self.packets_sending[:]:
+            if p.seq_num <= ack_num:
+                t.cancel()
+                self.packets_sending.remove((p, t))
+
